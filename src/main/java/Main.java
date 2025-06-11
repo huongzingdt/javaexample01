06b4543dd0e313f6ac43b935767c8661772d1208
huongdt <huongzingdt@gmail.com>
2025-06-11 11:20:30 +0700

    fix required

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("cheking lan 2");
         int a  = 1;
         int b = 5;
-        System.out.println(a+b);
+        System.out.println("Ket qua a + b = " +(a+b));
     }
 }
