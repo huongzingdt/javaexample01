53043b123fb628250f153ef848f786fc5212ae4d
huongdt <huongzingdt@gmail.com>
2025-06-11 10:48:08 +0700

    commit nhanh feature\

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Checking");
         System.out.println("cheking lan 2");
+        int a  = 1;
+        int b = 5;
+        System.out.println(a+b);
     }
 }
